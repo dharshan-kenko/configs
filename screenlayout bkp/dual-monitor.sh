beed316d59173cbbe9c9b8681a3544d84a6f3122
Dharshan K <112863919+dharshan-kenko@users.noreply.github.com>
2023-02-03 09:55:40 +0530

    Added arandr layout files

---
+++ screenlayout bkp/dual-monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --mode 1920x1080 --pos 2560x0 --rotate left --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --mode 2560x1080 --pos 0x296 --rotate normal --output VIRTUAL1 --off
